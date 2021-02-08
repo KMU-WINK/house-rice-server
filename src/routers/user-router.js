c800428fce3b6ab45aeb7b97c68a99552b278068
bell2lee <paperlee@kookmin.ac.kr>
2021-02-08 21:53:26 +0900

    회원 정보 전체 수정 구현

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -22,6 +22,24 @@ router.post('/', (req, res) => {
     res.status(200).json({ test: 'test' });
 });
 
+router.put('/:username', async (req, res) => {
+    const {
+        password, nickname, name, phone,
+    } = req.body;
+    const isModified = await userService.updateUserFullField({
+        username: req.params.username,
+        password,
+        nickname,
+        name,
+        phone,
+    });
+    if (isModified) {
+        res.status(200).json({ msg: '성공적으로 수정되었습니다.' });
+    } else {
+        res.status(404).json({ msg: '존재하지 않는 회원입니다.' });
+    }
+});
+
 router.get('/:id', (req, res) => {
     // 한 명의 유저만 가져옵니다.
 });
