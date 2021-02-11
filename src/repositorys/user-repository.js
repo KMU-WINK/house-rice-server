5b9f324a336cd62891be097c20d4ce6147ef30a7
Sanglim00 <sanglim@kookmin.ac.kr>
2021-02-11 18:59:54 +0900

    encrypted Password

--- src/repositorys/user-repository.js
+++ src/repositorys/user-repository.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
 
 mongoose.connect('mongodb://127.0.0.1:27017/house-rice', {
     useNewUrlParser: true,
@@ -31,6 +34,20 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+});
+
+UserSchema.pre('register', function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    }
 });
 
 const User = mongoose.model('User', UserSchema);
