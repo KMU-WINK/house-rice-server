d637a982f3a580ba8e2dcd5afa20f9e55d215246
hwna00 <layton3534@gmail.com>
2021-02-16 22:49:22 +0900

    회원정보 수정 구현

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,44 +1,55 @@
 import UserRepository from '../repositorys/user-repository';
 
 export async function register(args) {
-    /*
-    * args.name
-    * args.username
-    * args.password
-    * args.nickname
-    * args.phone
-    * */
-    const {
-        name, username, password, nickname, phone,
-    } = args; // args 에서 왼쪽 내용들을 꺼낸다.
-    if (!await UserRepository.findOne({ username })) {
-        const newUser = new UserRepository({
-            name,
-            username,
-            password,
-            nickname,
-            phone,
-        });
-        await newUser.save();
-    } else {
-        // throw Error('~~~~')
-    }
+  /*
+   * args.name
+   * args.username
+   * args.password
+   * args.nickname
+   * args.phone
+   * */
+  const {name, username, password, nickname, phone} = args; // args 에서 왼쪽 내용들을 꺼낸다.
+  if (!(await UserRepository.findOne({username}))) {
+    const newUser = new UserRepository({
+      name,
+      username,
+      password,
+      nickname,
+      phone,
+    });
+    await newUser.save();
+  } else {
+    // throw Error('~~~~')
+  }
 }
 
 export async function getUsers() {
-    const rawUsers = await UserRepository.find();
-    const users = rawUsers.map((row) => {
-        const {
-            name, username, nickname, phone,
-        } = row;
-        return {
-            // eslint-disable-next-line no-underscore-dangle
-            id: row._id,
-            name,
-            username,
-            nickname,
-            phone,
-        };
-    });
-    return users || null; // 값이 있으면 보내고 없으면 null을 보내라.
+  const rawUsers = await UserRepository.find();
+  const users = rawUsers.map((row) => {
+    const {name, username, nickname, phone} = row;
+    return {
+      // eslint-disable-next-line no-underscore-dangle
+      id: row._id,
+      name,
+      username,
+      nickname,
+      phone,
+    };
+  });
+  return users || null; // 값이 있으면 보내고 없으면 null을 보내라.
+}
+
+export async function updateUser(args) {
+  const {username, password, nickname, name, phone} = args;
+  const user = await UserRepository.findOne({username});
+
+  if (!user) return false;
+
+  if (password) user.password = password;
+  if (nickname) user.nickname = nickname;
+  if (name) user.name = name;
+  if (phone) user.phone = phone;
+
+  await user.save();
+  return true;
 }
